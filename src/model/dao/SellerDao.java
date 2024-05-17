519bfde554cfe2d921aaf0040ee5a6e596d69b72
Thiago-Souzaa <tssti@outlook.com>
2024-05-17 13:11:06 -0300

    insert implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -11,7 +11,8 @@ public interface SellerDao {
 	void update(Seller obj);
 	void deleteById(Integer id);
 	Seller findById(Integer id);
-	List<Department> findAll();
+	//List<Department> findAll();
+	List<Seller> findAll();
 	List<Seller> findByDepartment(Department department);
 	
 	//ss 
